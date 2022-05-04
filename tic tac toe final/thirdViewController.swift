84e42f3cfc1b25f5620b8c9042dff3b7d41edd8e
joey sokan <jsokan5343@stu.d214.org>
2022-05-04 14:36:04 -0500

    1

--- tic tac toe final/thirdViewController.swift
+++ tic tac toe final/thirdViewController.swift
@@ -8,22 +8,64 @@
 import UIKit
 
 class thirdViewController: UIViewController {
+  
+    enum Turn {
+    case Nought
+    case Cross
+    }
+    
+    
 
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var topOne: UIButton!
+    @IBOutlet weak var topTwo: UIButton!
+    @IBOutlet weak var topThree: UIButton!
+    @IBOutlet weak var middleOne: UIButton!
+    @IBOutlet weak var middleTwo: UIButton!
+    @IBOutlet weak var middleThree: UIButton!
+    @IBOutlet weak var bottomOne: UIButton!
+    @IBOutlet weak var bottomTwo: UIButton!
+    @IBOutlet weak var bottomThree: UIButton!
+    
+    
+    var firstTurn = Turn.Cross
+    var currentTurn = Turn.Cross
+    
+    var NOUGHT = "O"
+    var CROSS = "X"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+    
     }
     
 
-    /*
-    // MARK: - Navigation
+@IBAction func boardTapAction(_ sender: UIButton) {
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+    addToBoard(sender)
 
+}
+    
+    func addToBoard(_ sender: UIButton)
+    {
+        if(sender.title(for: .normal) == nil)
+        {
+            if(currentTurn == Turn.Nought)
+            {
+                sender.setTitle(NOUGHT, for: .normal)
+                currentTurn = Turn.Cross
+                turnLabel.text = CROSS
+            }
+            else if(currentTurn == Turn.Cross)
+            {
+                sender.setTitle(CROSS, for: .normal)
+                currentTurn = Turn.Nought
+                turnLabel.text = NOUGHT
+            
+        
+        }
+        }
+}
 }
