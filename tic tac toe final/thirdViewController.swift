cf44644084c1d567f387c5b2b9ea24729b8c1cac
joey sokan <jsokan5343@stu.d214.org>
2022-05-12 14:46:04 -0500

    start of sound effects code

--- tic tac toe final/thirdViewController.swift
+++ tic tac toe final/thirdViewController.swift
@@ -6,7 +6,7 @@
 //
 
 import UIKit
-
+import AVFoundation
 class thirdViewController: UIViewController {
   
     
@@ -182,6 +182,21 @@ class thirdViewController: UIViewController {
         }
             sender.isEnabled = false
         }
+        
+        var audioPlayer: AVAudioPlayer?
+        
+        func playSound(sound:String, type: String) {
+            if let path = Bundle.main.path(forResource: sound, ofType: type){
+                do {
+        audioPlayer = try AVAudioPlayer(contentsOf: URL (fileURLWithPath: path))
+                    audioPlayer?.play()
+                } catch {
+                    print("ERROR: Could not find and play the sound file!")
+                    
+                }
+        
 }
 }
 
+    }
+}
