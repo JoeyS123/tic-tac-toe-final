806a0d398130c7f2968910cd8b32b9c27bd3b5d0
joey sokan <jsokan5343@stu.d214.org>
2022-05-18 14:03:12 -0500

    getting rid  of red code

--- tic tac toe final/fourthViewController.swift
+++ tic tac toe final/fourthViewController.swift
@@ -13,28 +13,6 @@ class fourthViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        func BindableObject(_ sender: UIButton) {
-            
-        enum SquareStatus {
-            case empty
-            case visitor
-            case home
-        }
-        class Square: BindableObject {
-            let didChange = PassthroughSubject<Void, Never>()
-            
-            var status: SquareStatus {
-                didSet {
-                    didChange.send(())
-                }
-            }
-            
-            init(status: SquareStatus) {
-                self.status = status
-            }
-        }
-            et didChange = PassthroughSubject <Void, Never> ()
-        
         
         
     }
