58256aae7d38afca6d5587fc141e8d3a1260013c
joey sokan <jsokan5343@stu.d214.org>
2022-05-18 14:52:49 -0500

    Continued code for AI

--- tic tac toe final/fourthViewController.swift
+++ tic tac toe final/fourthViewController.swift
@@ -5,7 +5,7 @@
 //  Created by Logan Peterson on 5/12/22.
 //
 
-import UIKit
+import SwiftUI
 
 class fourthViewController: UIViewController {
 
@@ -14,6 +14,59 @@ class fourthViewController: UIViewController {
         super.viewDidLoad()
 
         
+        enum SquareStatus {
+            case empty
+            case home
+            case visitor
+        }
+        class Sqaure : ObservableObject {
+            @Published var squareStatus : SquareStatus
+            
+            init(status : SquareStatus) {
+                self.squareStatus = status
+            }
+        }
+        class TicTacToeModel : ObservableObject {
+            @Published var squares = [Sqaure]()
+            init() {
+                for _ in 0...8{
+                    squares.append(Sqaure(status:.empty))
+                }
+            }
+            
+        }
+        struct SquareView : View {
+            var body: some View {
+                Button(action: {
+                    
+                }, label: {
+                    Text("Button")
+                })
+                
+            }
+        }
+        struct ContentView: View {
+            @StateObject var ticTacToeModel = TicTacToeModel()
+            var body: some View {
+                ForEach(0 ..<  ticTacToeModel.squares.count / 3, content: {
+                    row in
+                    HStack {
+                        ForEach(0 ..< 3, content: {
+                            column in
+                            Color.gray.frame(width: 70, height: 70, alignment: .center)
+                        })
+                    }
+                })
+            }
+            
+            
+        }
+        
+        struct ContentView_Previews:PreviewProvider {
+            static var previews: some View {
+                ContentView()
+            }
+        }
         
     }
     
@@ -21,4 +74,5 @@ class fourthViewController: UIViewController {
     
 
 }
-}
+
+
