6600f3b0e1369b32be21109f264a1394a571774d
Logan Peterson <lpeterson4345@stu.d214.org>
2022-05-20 14:16:50 -0500

    made new view controller and fixed the spacing on the main

---
+++ tic tac toe final/extraViewController.swift
@@ -0,0 +1,29 @@
+//
+//  extraViewController.swift
+//  tic tac toe final
+//
+//  Created by Logan Peterson on 5/20/22.
+//
+
+import UIKit
+
+class extraViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
